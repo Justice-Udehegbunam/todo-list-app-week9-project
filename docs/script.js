49cca8f7948633cf363af68d33c0a37fdd0a30eb
Justice-Udehegbunam <justicefavour491@gmail.com>
2023-09-25 14:06:25 +0100

    Javascript workflow  - The pseudocodes.

---
+++ docs/script.js
@@ -0,0 +1,8 @@
+// Create DOM elements
+// Create a function to create a new task Item
+//Create a span for the task description
+//Create a span for the task date and time
+// Add an event listner to mark a task as completed
+// Add a delete button
+// Add an onclick event to delete a task
+// Add a new task
